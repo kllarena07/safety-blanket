ae414094cf952f058a55f600e53acb8d75f28b3c
Kieran Llarena <baymax709@gmail.com>
2024-05-26 01:57:58 -0700

    wtf is going on

--- frontend/src/app/trip/page.tsx
+++ frontend/src/app/trip/page.tsx
@@ -8,7 +8,7 @@ export default function Timer() {
   return (
     <main className="flex flex-col white bg-[#E4EDEE] h-dvh w-full">
       <header className="py-5 bg-white text-center">
-        <h1 className="text-3xl bg-white text-black font-bold">Timer</h1>
+        <h1 className="text-3xl bg-white text-black font-bold">Trip</h1>
       </header>
       <section className="flex flex-col h-[calc(100dvh-164px)] items-center justify-center gap-5">
         <button onClick={handleEndTrip} className="bg-[#CC7178] text-black font-bold w-[200px] h-[200px] rounded-full">End Trip</button>
