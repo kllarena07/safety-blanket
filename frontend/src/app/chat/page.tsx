79a05e0a53ccff8a2185fc9e12936cff009e43aa
Bill Zhang <billzhangsc@gmail.com>
2024-05-26 04:10:29 -0700

    More

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -9,11 +9,11 @@ export default function Chat() {
   const [inputValue, setInputValue] = useState("");
   const [msgSocket, setMsgSocket] = useState(null);
   const chatRef = useRef(null);
-  
+
   useEffect(() => {
     const messageWs = new WebSocket('ws://127.0.0.1:8000/message-ws?client_id=123');
     const checkInWs = new WebSocket('ws://127.0.0.1:8000/checkin-ws?client_id=123');
-    
+
     messageWs.onopen = () => {
       console.log('Message WS is open now.');
       setMsgSocket(messageWs);
@@ -43,7 +43,7 @@ export default function Chat() {
     checkInWs.onclose = () => {
       console.log('WebSocket is closed now.');
     };
-    
+
     return () => {
       messageWs.close();
       checkInWs.close();
@@ -60,7 +60,7 @@ export default function Chat() {
 
     msgSocket.send(JSON.stringify(data));
     setInputDisabled(true);
-    setMessages(prevMessages => [ ...prevMessages, data ]);
+    setMessages(prevMessages => [...prevMessages, data]);
     setInputValue("");
   }
 
@@ -75,37 +75,30 @@ export default function Chat() {
       </header>
       <section className="flex flex-col p-5 h-[calc(100dvh-76px)]">
         <section className="p-2 bg-[#232323] rounded-3xl h-[409px]" ref={chatRef}>
-            <ul className="overflow-y-scroll h-[calc(100%-44px)] flex flex-col">
-              {messages.map(({ owner, content }, index) => (
-                <li key={index} className={`${owner === "user" ? "self-end bg-[#FFDBDB]" : "self-start bg-[#CC7178]"} flex flex-col w-1/2 py-2 px-5 rounded-full text-black mb-5`}>
-                  <p key={index}>{content}</p>
-                </li>
-              ))}
-              {inputDisabled && msgSocket !== null ? (
-                <li className="self-start bg-[#CC7178] flex flex-col w-1/2 py-2 px-5 rounded-full text-black mb-5">
-                  ...
-                </li>
-              ): ' '}
-            </ul>
-            <section className="flex w-full rounded-full bg-white overflow-hidden items-center">
-              <input disabled={inputDisabled} onChange={(evt) => setInputValue(evt.target.value)} value={inputValue} placeholder="Send a message" type="text" className="w-full text-black p-2.5 disabled:cursor-not-allowed" onKeyDown={handleKeyDown} />
-              <button className="mr-5" onClick={() => appendNewMessage(inputValue)}>
-                <Arrow className="size-5" />
-              </button>
-            </section>
+          <ul className="overflow-y-scroll h-[calc(100%-44px)] flex flex-col">
+            {messages.map(({ owner, content }, index) => (
+              <li key={index} className={`${owner === "user" ? "self-end bg-[#FFDBDB]" : "self-start bg-[#CC7178]"} flex flex-col w-1/2 py-2 px-5 rounded-full text-black mb-5`}>
+                <p key={index}>{content}</p>
+              </li>
+            ))}
+            {inputDisabled && msgSocket !== null ? (
+              <li className="self-start bg-[#CC7178] flex flex-col w-1/2 py-2 px-5 rounded-full text-black mb-5">
+                ...
+              </li>
+            ) : ' '}
+          </ul>
+          <section className="flex w-full rounded-full bg-white overflow-hidden items-center">
+            <input disabled={inputDisabled} onChange={(evt) => setInputValue(evt.target.value)} value={inputValue} placeholder="Send a message" type="text" className="w-full text-black p-2.5 disabled:cursor-not-allowed" onKeyDown={handleKeyDown} />
+            <button className="mr-5" onClick={() => appendNewMessage(inputValue)}>
+              <Arrow className="size-5" />
+            </button>
+          </section>
         </section>
         <section className="bg-[#CC7178] text-black font-bold p-5 rounded-full my-5">
           Next Check-in
         </section>
 
-        <footer className="flex rounded-full justify-between bg-white p-3">
-          <button className="text-[#843232] bg-[#FFDBDB] rounded-full font-bold px-10 py-5 w-[80%]">Call Companion</button>
-          <a href='/profile' className="bg-[#CC7178] rounded-full p-2.5">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="h-full">
-              <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-            </svg>
-          </a>
-        </footer>
+
       </section>
     </main>
   )
