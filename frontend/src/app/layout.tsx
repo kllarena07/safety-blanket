e30d95ed2c7f1d2a3ae4adb771cf7815ba5ffde4
Bill Zhang <billzhangsc@gmail.com>
2024-05-26 03:28:26 -0700

    bruh

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { Inter } from "next/font/google";
 import { ClerkProvider, useUser } from "@clerk/nextjs";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +14,11 @@ export default function RootLayout({
 
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>{children}
+          <Navbar />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,15 +9,13 @@ export default function Navbar() {
     <>
       {isToggled ? (
         <section className="absolute w-full top-[76px] bg-black flex flex-col h-[calc(100dvh-164px)] items-center justify-center gap-5">
-        <a href='settings'>Home</a>
-        <a href='settings'>Chat</a>
-        <a href='settings'>Settings</a>
-      </section>
+          <a href='settings'>Home</a>
+          <a href='settings'>Chat</a>
+          <a href='settings'>Settings</a>
+        </section>
       ) : ''}
-      <section className="flex flex-col h-[calc(100dvh-164px)] items-center justify-center gap-5">
-        <button onClick={() => setToggle(!isToggled)} className="bg-[#CC7178] text-black font-bold w-[200px] h-[200px] rounded-full">End Trip</button>
-      </section>
-      <footer className="flex rounded-full justify-between bg-white p-3 mx-5 mb-5">
+
+      <footer className="flex rounded-full justify-between bg-white p-3 mx-5 mb-5 fixed bottom-0 left-0">
         <button className="text-[#843232] bg-[#FFDBDB] rounded-full font-bold px-10 py-5 w-[80%]">Call Companion</button>
         <button onClick={() => setToggle(!isToggled)} className={`bg-[#CC7178] rounded-full p-2.5 transition-transform ${isToggled ? "rotate-90" : ''}`}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-full">
