73227f0c99d1e2e6695571ef6b805c6190c01797
Kieran Llarena <baymax709@gmail.com>
2024-05-26 07:35:59 -0700

    refactor: always have call companion enabled

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
-import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export default function Navbar() {
-  const pathname = usePathname();
   const [isToggled, setToggle] = useState(false);
   const { user } = useUser();
   const userPhone = user?.primaryPhoneNumber?.phoneNumber;
 
-  console.log(pathname);
 
   const handleClick = () => {
     fetch(`https://7771014229a3.ngrok.app/phone/${userPhone}`)
@@ -28,7 +25,7 @@ export default function Navbar() {
         </section>
       ) : ''}
       <footer className="flex rounded-full justify-between bg-white p-3 mx-5">
-        <button disabled={pathname === '/' ? true : false} className="disabled:opacity-50 disabled:cursor-not-allowed text-[#843232] bg-[#FFDBDB] rounded-full font-bold px-10 py-5 w-[80%]" onClick={() => handleClick()}>Call Companion</button>
+        <button className="text-[#843232] bg-[#FFDBDB] rounded-full font-bold px-10 py-5 w-[80%]" onClick={() => handleClick()}>Call Companion</button>
         <button onClick={() => setToggle(!isToggled)} className="flex items-center justify-center bg-[#CC7178] w-[64px] h-[64px] rounded-full p-2.5">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
